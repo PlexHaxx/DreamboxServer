307692b184484e49b4a965f6debab234b16d414c
greeeny101 <marc.greenwood574@gmail.com>
2015-01-15 07:55:22 +0000

    ffmpeg presets added

--- Server/Server.py
+++ Server/Server.py
@@ -2,8 +2,27 @@ __author__ = 'marcgreenwood'
 
 
 import cherrypy
+import os
 from cherrypy.lib.static import serve_file
 
+
+path = os.path.abspath(os.path.dirname(__file__))
+config = {
+  'global' : {
+    'server.socket_host' : 'localhost',
+    'server.socket_port' : 9090,
+    'server.thread_pool' : 8
+  },
+  '/bootstrap' : {
+    'tools.staticdir.on'            : True,
+    'tools.staticdir.dir'           : os.path.join(path, 'bootstrap'),
+    'tools.staticdir.content_types' : {'html': 'text'}
+  },
+  '/style':{
+    'tools.staticdir.on'            : True,
+    'tools.staticdir.dir'           : os.path.join(path, 'style')
+  }
+}
 
 
 class Home():
@@ -17,7 +36,12 @@ class Home():
 
 
 
-cherrypy.quickstart(Home(), config='config.conf')
+cherrypy.quickstart(Home(), config=config)
 """
-ffmpeg -i http://192.168.1.252:8001/1:0:1:1933:7FF:2:11A0000:0:0:0: -b:v 128k -force_key_frames 2  -f segment -segment_list_flags +live -segment_list live_manifest.m3u8 -segment_time 2 out%03d.ts
+
+
+ffmpeg  -i http://192.168.1.252:8001/1:0:19:1B1D:802:2:11A0000:0:0:0: -override_ffserver -map 0:0 -map 0:15 -s 1920x1080 -buffer_size 160000000  -vcodec libx264  -crf 30 -pix_fmt yuv420p -vb 1500000  -preset ultrafast  -strict experimental -vf yadif -acodec libfaac -ab 96000 -ac 2  http://192.168.1.100:28090/feed4.ffm
+
+
+ffmpeg  -i http://192.168.1.252:8001/1:0:19:1B1D:802:2:11A0000:0:0:0: -override_ffserver -map 0:0 -map 0:15 -s 1920x1080 -buffer_size 160000000  -vcodec h264  -flags +loop+mv4 -cmp 256 -partitions +parti4x4+parti8x8+parti4x4+part8x8 -subq 7 -trellis 1 -refs 5 -bf 0  -coder 0 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -qmin 10 -qmax 51 -qdiff 4 -vb 1500000  -preset ultrafast  -strict experimental -vf yadif -acodec libfaac -ab 96000 -ac 2  http://192.168.1.100:28090/feed4.ffm
 """

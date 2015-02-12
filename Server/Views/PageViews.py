ddacd95b1871643454d512d986e15048ff090268
marc <mgreenwood@fisconline.co.uk>
2015-02-12 13:21:57 +0000

    Updates

--- Server/Views/PageViews.py
+++ Server/Views/PageViews.py
@@ -16,15 +16,17 @@ from Plugins.NowNextMonitor import NowNextMonitor
 
 MessageRequest = namedtuple('MessageRequest', 'script, action, data', verbose=False)
 
-class PageBase(object):
-    #TODO Implement this in other page views
 
-    #Record any misssing params
+class PageBase(object):
+    # TODO Implement this in other page views
+
+    # Record any misssing params
     missing_params = {}
-    #Params that are not the correct format
-    incorrect_params{}
-    #Params that are ok. Stored in dict se we are not constantly going to the server and fetching values. Just update this after a value has been saved ok in the database
-    params ={}
+    # Params that are not the correct format
+    incorrect_params = {}
+    # Params that are ok. Stored in dict se we are not constantly going to the server and fetching values.
+    # Just update this after a value has been saved ok in the database
+    params = {}
 
     @classmethod
     def check_params(cls, params):
@@ -32,8 +34,7 @@ class PageBase(object):
         #TODO add to params as theyt are ok - Incorrrect checked as we update them - Do we actually need tyhat as we can check before saving <-- Yes do this
 
 
-class API():
-
+class API(PageBase):
     def __init__(self):
         self.q = PriorityQueue()
         cherrypy.engine.subscribe('api', self.dispatch)
@@ -62,7 +63,7 @@ class API():
 
     @cherrypy.expose
     def get_channels(self, bouquet_id):
-        #todo validate sref
+        # todo validate sref
         cherrypy.engine.publish('db_handler', MessageRequest('api', 'get_channels', bouquet_id))
         for i in xrange(1, 100, 1):
             resp = self.q.get()
@@ -71,29 +72,29 @@ class API():
 
     @cherrypy.expose
     def get_channel_now_next(self, channel_id):
-        #todo validate sref
+        # todo validate sref
         cherrypy.engine.publish('db_handler', MessageRequest('api', 'get_channel_now_next', channel_id))
         for i in xrange(1, 100, 1):
             resp = self.q.get()
             return resp.data if resp.action == 'get_channel_now_next' else self.q.put(resp)
         return None
 
+
 class Feeds():
 
     def __init__(self, path, host):
-
         self.path = path
         self.host = host
 
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-
-        channels = [('audio1', 'http://{}:{}/live_audio1.mkv'.format(self.path, self.host)), 
-                           ('audio2', 'http://{}:{}/live_audio2.mkv'.format(self.path, self.host))]
+        channels = [('audio1', 'http://{}:{}/live_audio1.mkv'.format(self.path, self.host)),
+                    ('audio2', 'http://{}:{}/live_audio2.mkv'.format(self.path, self.host))]
         return channels
 
-class Stream():
+
+class Stream(PageBase):
 
     def __init__(self, path, host):
 
@@ -112,18 +113,17 @@ class Stream():
 
     @cherrypy.expose
     def audio1(self):
-        #todo need to publish methods here to stop, then start so we dont return straight away
+        # todo need to publish methods here to stop, then start so we dont return straight away
         cherrypy.engine.publish('transcoder', MessageRequest('stream', 'start_transcoder', '1'))
         for i in xrange(1, 100, 1):
             cherrypy.log('waiting for response response')
             resp = self.q.get()
             cherrypy.log('got response')
-            if resp.action == 'start_transcoder' :
+            if resp.action == 'start_transcoder':
                 raise cherrypy.HTTPRedirect('http://localhost:28090/live_audio1.mkv')
             else:
                 self.q.put(resp)
         return None
-
 
 
     @cherrypy.expose
@@ -132,106 +132,115 @@ class Stream():
         raise cherrypy.HTTPRedirect('http://localhost:28090/live_audio2.mkv')
 
 
-
-class About():
-
+class About(PageBase):
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/about.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/about.html', lookup=mylookup)
         return index.render()
 
-class Player():
+
+class Player(PageBase):
+
+    def __init__(self, params):
+        super(Player, self).__init__()
+        self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
+        Player.check_params(self.params)
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/player.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/player.html', lookup=mylookup)
         return index.render()
+
 
 class Dreambox(PageBase):
 
     def __init__(self, params):
-
-
+        super(Dreambox, self).__init__()
         self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
-        Dreambox.check_params( self.params)
+        Dreambox.check_params(self.params)
 
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/dreambox.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/dreambox.html', lookup=mylookup)
         return index.render(**self.params)
 
-class FFMPEG():
+
+class FFMPEG(PageBase):
 
     def __init__(self, params):
+
+        super(FFMPEG, self).__init__()
         self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/ffmpeg.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/ffmpeg.html', lookup=mylookup)
         return index.render(**self.params)
 
-class FFServer():
+
+class FFServer(PageBase):
 
     def __init__(self, params):
+
+        super(FFServer, self).__init__()
         self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/ffserver.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/ffserver.html', lookup=mylookup)
         return index.render(**self.params)
 
-class Plex():
+
+class Plex(PageBase):
 
     def __init__(self, params):
+        super(Plex, self).__init__()
         self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/plex.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/plex.html', lookup=mylookup)
         return index.render(**self.params)
 
-class DreamboxServer():
+
+class DreamboxServer(PageBase):
 
     def __init__(self, params):
+
+        super(DreamboxServer, self).__init__()
         self.params = dict((k, v) for item in params for (k, v) in item.iteritems())
 
     @cherrypy.expose
     def index(self):
-        #todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
+        # todo Some subscribe call here and elsewhere to put message on bus we have selected something and call appropriate plugin
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
-        index = Template (filename=os.path.abspath('.') + '/html/dreamboxserver.html', lookup=mylookup)
+        index = Template(filename=os.path.abspath('.') + '/html/dreamboxserver.html', lookup=mylookup)
         return index.render(**self.params)
 
 
 class Home(PageBase):
 
-
     def __init__(self):
 
         super(Home, self).__init__()
-
-
         self.q = PriorityQueue()
         DBHandler(cherrypy.engine).subscribe()
         cherrypy.engine.subscribe('server', self.dispatch)
         self.get_params()
-
-
-
 
     def start_webapp_handlers(self, params):
         """
@@ -244,7 +253,7 @@ class Home(PageBase):
         """
         Home.Player = Player()
         Home.Dreambox = Dreambox(params.get('dreambox', {}))
-        Home.FFMPEG =FFMPEG(params.get('ffmpeg', {}))
+        Home.FFMPEG = FFMPEG(params.get('ffmpeg', {}))
         Home.FFServer = FFServer(params.get('ffserver', {}))
         Home.Plex = Plex(params.get('plex', {}))
         Home.DreamboxServer = DreamboxServer(params.get('dreamboxserver', {}))
@@ -292,8 +301,6 @@ class Home(PageBase):
             return None
 
 
-
-
     def dispatch(self, message):
 
         if isinstance(message, tuple):
@@ -301,8 +308,6 @@ class Home(PageBase):
         else:
             # we are waiting  for something
             self.q.put(message)
-
-
 
 
     @cherrypy.expose
@@ -316,8 +321,8 @@ class Home(PageBase):
         mylookup = TemplateLookup(directories=[os.path.abspath('.') + '/html'])
 
         if Home.missing_params:
-                index = Template (filename=os.path.abspath('.') + '/html/index_missing_vals.html', lookup=mylookup)
-                cherrypy.log(str(Home.missing_params))
+            index = Template(filename=os.path.abspath('.') + '/html/index_missing_vals.html', lookup=mylookup)
+            cherrypy.log(str(Home.missing_params))
         else:
             index = Template(filename=os.path.abspath('.') + '/html/index.html', lookup=mylookup)
 
@@ -326,7 +331,7 @@ class Home(PageBase):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def update_config(self, key, value):
-        #TODO Need to check here if value is empty, return false so we can display an error
+        # TODO Need to check here if value is empty, return false so we can display an error
         cherrypy.engine.publish('db_handler', MessageRequest('server', 'update_param', (key, value)))
         resulq = None
         for i in xrange(1, 100, 1):
